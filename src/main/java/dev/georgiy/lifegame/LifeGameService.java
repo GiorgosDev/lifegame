ae8d75f197aedec2de032faedf48d5f50684b594
Giorgos <mayshmaz@gmail.com>
2019-09-03 15:33:57 +0300

    Service added

--- src/main/java/dev/georgiy/lifegame/LifeGameService.java
+++ src/main/java/dev/georgiy/lifegame/LifeGameService.java
@@ -1,4 +1,18 @@
 package dev.georgiy.lifegame;
 
+import dev.georgiy.lifegame.beans.Point;
+import dev.georgiy.lifegame.utils.RoundUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Set;
+
+@RestController
 public class LifeGameService {
+
+    @RequestMapping(value = "/nextGeneration")
+    public Set<Point> getNextGeneration(Set<Point> pointSet) {
+        return RoundUtils.getNextGeneration(pointSet);
+    }
+
 }
